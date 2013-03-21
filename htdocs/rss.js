9ec4d048a89a58e1f2acb3ced5b38f9f846504e2
Shad Gregory <captshadg@gmail.com>
2013-03-21 14:56:14 -0500

    added blog list page

--- htdocs/rss.js
+++ htdocs/rss.js
@@ -29,12 +29,54 @@ function search() {
 		var entry_date = $(this).find('entry_date').text();
 		var url = $(this).find('url').text();
 		$('#results').append('<p><b>' + blog_title + '</b> ' +
-				     '<a href="' + url + '">' + 
-				     entry_title + " (" + entry_date + ")" +
-				     '</a></p>');
+				     '<a href="javascript:void(0)" onclick="window.open(\'' + url + '\')">' + 
+				     entry_title + " (" + entry_date +
+				     ')</a></p>');
 	    });
 	}
-
     });
+}
 
+function mark_read(feed_id, entry_id) {
+    $.ajax({
+	url : 'mark-read'
+	,data : {entry_id : entry_id} 
+	,context: document.body
+    });
+    $('#entry-'+entry_id).hide();
+    $.ajax({
+	url : 'get-feed-title'
+	,data: {feed_id : feed_id}
+	,content: document.body
+	,success: function(xml) {
+	    var title = $(xml).find('title').text();
+	    $('#blog_title_'+feed_id).text(title);
+	}
+    });
+}
+
+function retrieve_unread(feed_id) {
+
+    if (document.getElementById("results_"+feed_id).style.display == 'none') {
+	document.getElementById("results_"+feed_id).style.display = 'block';
+	$.ajax({
+	    url     : 'retrieve-unread'
+	    ,data   : {feed_id:feed_id}
+	    ,context: document.body
+	    ,success: function(xml){
+		$('#results_'+feed_id).html('');
+		$(xml).find("result").each(function(){
+		    var entry_title = $(this).find('title').text();
+		    var entry_desc = $(this).find('description').text();
+		    var entry_url = $(this).find('url').text();
+		    var entry_id = $(this).find('id').text();
+		    $('#results_'+feed_id).append('<div style="padding:4px;" id="entry-'+entry_id+'"><a onclick="mark_read('+feed_id + ","+entry_id+');window.open(\'' 
+						  + entry_url + '\');" href="javascript:void(0)">' 
+						  + entry_title + '</a></div>');
+		});
+	    }
+	});
+    } else {
+	document.getElementById("results_"+feed_id).style.display = 'none';
+    }
 }
--- rss.rkt
+++ rss.rkt
@@ -13,6 +13,19 @@
 		      #:database "feed"
 		      #:password "abc123"))
 
+;; string-append replacement
+(define-syntax str
+  (syntax-rules ()
+    ((_) "")
+    ((_ string1 . rest)
+     (cond
+       ((empty? string1)
+	(string-append "" (str . rest)))
+       ((number? string1)
+	(string-append (number->string string1) (str  . rest)))
+       (else
+	(string-append string1 (str . rest)))))))
+
 (define mark-read
   (lambda (req)
     (define bindings (request-bindings req))
@@ -28,26 +41,63 @@
     (define q (extract-binding/single 'q bindings))
     (response/xexpr
      `(results
-       ,@(for/list (((blog-title entry-title url entry-date) (in-query pgc 
-							    (string-append "select feed.title, entry.title, entry.url, entry.date from entry inner join feed on entry.feed_id = feed.id where lower(entry.title) like lower('%" q "%') or lower(entry.description) like lower('%" q "%') order by entry.date desc"))))
+       ,@(for/list (((blog-title entry-title url entry-date) 
+		     (in-query pgc 
+			       (string-append "select feed.title, entry.title, entry.url, entry.date from entry inner join feed on entry.feed_id = feed.id where lower(entry.title) like lower('%" q "%') or lower(entry.description) like lower('%" q "%') order by entry.date desc"))
+		     ))
 	  `(result
 	    (blog_title ,blog-title)
-	    (entry_date ,(string-append 
-			  (number->string (sql-timestamp-month entry-date)) "/"
-			  (number->string (sql-timestamp-day entry-date)) "/"
-			  (number->string (sql-timestamp-year entry-date))))
+	    (entry_date ,(str
+			  (sql-timestamp-month entry-date) "/"
+			  (sql-timestamp-day entry-date) "/"
+			  (sql-timestamp-year entry-date)))
 	    (entry_title ,entry-title)
 	    (url ,url))))
+     #:mime-type #"application/xml")))
+
+(define retrieve-unread
+  (lambda (req)
+    (define bindings (request-bindings req))
+    (define feed-id (extract-binding/single 'feed_id bindings))
+    (response/xexpr
+     `(results
+       ,@(for/list (((title description url id)
+		     (in-query pgc
+			       "select title, description, url, id from entry where feed_id=$1 and read=false limit 50" 
+			       (string->number feed-id))))
+	   `(result
+	     (title ,title)
+	     (description ,description)
+	     (url ,url)
+	     (id ,(number->string id))))))))
+
+(define blog-list
+  (lambda (req)
+    (response/xexpr
+     `(html
+       (body
+	,@(for/list (((title url id)
+		      (in-query pgc "select title, url, id from feed order by title")))
+	    (let ((unread-count (query-value pgc "select count(*) from entry where feed_id=$1 and read=false" id)))
+	      `(p (a ((id ,(str "blog_title_" id)) (onclick ,(str "retrieve_unread(" id ")")) 
+		      (href "javascript:void(0)")) ,(str title " (" unread-count ")"))
+		(div ((style "display:none;border:solid 1px black") (id 
+								     ,(str "results_" id))))))))))))
+(define get-feed-title
+  (lambda (req)
+    (define bindings (request-bindings req))
+    (define feed-id (extract-binding/single 'feed_id bindings))
+    (define unread-count (query-value pgc "select count(*) from entry where feed_id=$1 and read=false" (string->number feed-id)))
+    (define feed-title (query-value pgc "select title from feed where id=$1" (string->number feed-id)))
+    (response/xexpr
+     `(title ,(str feed-title " (" unread-count ")"))
      #:mime-type #"application/xml")))
 
 (define search-page
   (lambda (req)
     (response/xexpr
-     `(html
-       (head
-	(script ((src "//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"))" ")
-	(script ((type "text/javascript")(src "rss.js")) " "))
-       (body
+     `(html 
+      (body
 	(input ((name "rss_search") (id "rss_search") (onkeydown "if (event.keyCode == 13) search();")(type "text") (size "20")))
 	(button ((type "button") (onclick "search();")) "Search")
 	(div ((id "results"))))))))
@@ -57,33 +107,32 @@
     (response/xexpr
      `(html
        (head
+	(title "Shad's Reader")
 	(script ((type "text/javascript")(src "rss.js")) " ")
-	(link ((rel "stylesheet") (href "http://code.jquery.com/ui/1.10.2/themes/start/jquery-ui.css")) " ")
+	(link ((rel "stylesheet") (href "http://code.jquery.com/ui/1.10.2/themes/flick/jquery-ui.css")) " ")
 	(script ((src "//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js")) " ")
 	(script (( src "//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js")) " ")
-	(script "$(function() {$( '#tabs' ).tabs();});$('a').click(function() {$(this).attr('target', '_blank');});"))
-       (body ((bgcolor "e5e5e5"))
+	(script "$(function() {$( '#tabs' ).tabs();});"))
+       (body ((bgcolor "#5c9ccc") (style "background-color;#e5e5e5;"))
 	     (div ((id "tabs") (style "width:850px;margin-left:auto;margin-right:auto;"))
 		  (ul
 		   (li (a ((href "#latest_items")) "Latest Items"))
-		   (li (a ((href "search-page")) "Search")))
+		   (li (a ((href "search-page")) "Search"))
+		   (li (a ((href "blog-list")) "Blog List")))
 		  (div ((id "latest_items"))
 		       ,@(for/list (((feed-title title link image-url date desc entry-id) 
 				     (in-query pgc 
-					       "select feed.title, entry.title, entry.url, feed.image_url, entry.date, entry.description, entry.id from entry inner join feed on feed.id = entry.feed_id where entry.date > (now () - interval '48 hour') and entry.read = false order by entry.date desc")))
+					       "select feed.title, entry.title, entry.url, feed.image_url, entry.date, entry.description, entry.id from entry inner join feed on feed.id = entry.feed_id where entry.date > (now () - interval '36 hour') and entry.read = false order by entry.date desc")))
 			   `(p (div ,(string-append 
 				      (number->string (sql-timestamp-month date)) "/"
 				      (number->string (sql-timestamp-day date)) "/"
-				      (number->string (sql-timestamp-year date))
-				      " ")
-				    (b ,feed-title)) (a ((href ,link)) ,title)
+				      (number->string (sql-timestamp-year date)) " ")
+				    (b ,feed-title)) (a ((href "javascript:void(0)") (onclick  ,(str "window.open('"  link "')"))) ,title)
 				    (div (a ((href "javascript:void(0)") 
 					     (id ,(string-append "toggle-" (number->string entry-id))) 
 					     (onclick ,(string-append "flip('toggle-" (number->string entry-id) "', 'desc-" (number->string entry-id) "');")))"Show"))
 				    (div ((style "display:none")(id ,(string-append "desc-" (number->string entry-id)))) 
-					 ,(cdata 'cdata-start 'cdata-end desc))
-				    )
-			   ))))))))
+					 ,(cdata 'cdata-start 'cdata-end desc)))))))))))
 
 (define (start request)
   (rss-dispatch request))
@@ -93,8 +142,10 @@
    (("") recent-entries)
    (("mark-read") mark-read)
    (("search") search)
+   (("blog-list") blog-list)
    (("search-page") search-page)
-   ))
+   (("get-feed-title") get-feed-title)
+   (("retrieve-unread") retrieve-unread)))
 
 (serve/servlet start
 	       #:launch-browser? #f
