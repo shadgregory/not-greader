f814f84c1759ed24f6520e46c9a8856fd663d23a
Shad Gregory <captshadg@gmail.com>
2013-04-10 03:51:24 -0500

    support for atom

--- rss.pl
+++ rss.pl
@@ -3,6 +3,8 @@ use strict;
 use LWP::Simple;
 use DBI;
 use XML::RSS;
+use XML::Feed;
+use Data::Dumper;
 
 my @feeds = ();
 my $dbh = DBI->connect("dbi:Pg:dbname=feed", "feed","abc123");
@@ -10,7 +12,7 @@ my $dbh = DBI->connect("dbi:Pg:dbname=feed", "feed","abc123");
 #clean up the oldest items
 $dbh->do("UPDATE item SET read=true where date < (now() - interval '6 month')");
 
-my $feed_handle = $dbh->prepare("select id, url, title from feed;");
+my $feed_handle = $dbh->prepare("select id, url, title from feed");
 $feed_handle->execute();
 
 while (my @feeds = $feed_handle->fetchrow_array()) {
@@ -20,19 +22,35 @@ while (my @feeds = $feed_handle->fetchrow_array()) {
     my $xml = get($feed_url);
     my $rss = new XML::RSS;
     $rss->parse($xml);
-    for my $i (@{$rss->{items}}) {
-	my $pub_date = $i->{'pubDate'};
-	$pub_date = $i->{'dc'}->{'date'} if !$pub_date;
-	my $title    = $i->{'title'};
-	my $url      = $i->{'link'};
-	my $desc     = $i->{'description'};
-	next if !$url; #looking at you perl 6 planet!
-	my $sth = $dbh->prepare("SELECT * from item where url=?;");
-	$sth->execute($url);
-	my @data = $sth->fetchrow_array();
-	next if @data > 0;
-	my $insert_sth = $dbh->prepare("INSERT INTO item(title, url, feed_id, description, date) values(?,?,?,?,?)");
-	$insert_sth->execute($title, $url, $feed_id, $desc, $pub_date);
+    if (@{$rss->{items}} > 0) {
+	for my $i (@{$rss->{items}}) {
+	    my $pub_date = $i->{'pubDate'};
+	    $pub_date    = $i->{'dc'}->{'date'} if !$pub_date;
+	    my $title    = $i->{'title'};
+	    my $url      = $i->{'link'};
+	    my $desc     = $i->{'description'};
+	    next if !$url; #looking at you perl 6 planet!
+	    my $sth = $dbh->prepare("SELECT * from item where url=?;");
+	    $sth->execute($url);
+	    my @data = $sth->fetchrow_array();
+	    next if @data > 0;
+	    my $insert_sth = $dbh->prepare("INSERT INTO item(title, url, feed_id, description, date) values(?,?,?,?,?)");
+	    $insert_sth->execute($title, $url, $feed_id, $desc, $pub_date);
+	}
+    } else {
+	my $feed = XML::Feed->parse(URI->new($feed_url));
+	foreach ($feed->entries) {
+	    my $title    = $_->title;
+	    my $pub_date = $_->updated;
+	    my $desc = $_->content->body;
+	    my $url      = $_->link;
+	    my $sth = $dbh->prepare("SELECT * from item where url=?;");
+	    $sth->execute($url);
+	    my @data = $sth->fetchrow_array();
+	    next if @data > 0;
+	    my $insert_sth = $dbh->prepare("INSERT INTO item(title, url, feed_id, description, date) values(?,?,?,?,?)");
+	    $insert_sth->execute($title, $url, $feed_id, $desc, $pub_date);
+	}
     }
 }
 
